dde42cc8d98dbe7f5fa216317482f998ca43b907
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-06-13 00:11:10 +0300

    add button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from "react";
+import "./Button.scss";
+
+type ButtonProps = {
+  text?: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  className: string;
+  disabled?: boolean;
+  icon?: ReactElement;
+};
+export const Button = ({
+  onClick,
+  text,
+  className,
+  disabled,
+  icon,
+}: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`${className} button`}
+      disabled={disabled}
+    >
+      {icon}
+      {text}
+    </button>
+  );
+};
