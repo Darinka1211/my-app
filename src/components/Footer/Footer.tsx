91a2c93e8e57b69de7e59edc974a8c01589727dc
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-06-13 01:09:36 +0300

    add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Footer.scss'
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer--main">
+                <p>2022 Blogofolio</p>
+            </div>
+            <p>All rights reserved</p>
+        </footer>
+    )
+}
